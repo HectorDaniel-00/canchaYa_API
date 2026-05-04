17953a8c4001321fc1be178f04704d25f1aa9a81
HectorDaniel-00 <danielvargas.nike@gmail.com>
2026-05-04 10:57:11 -0500

    feat: Modulo users

--- app/src/modules/users/users.module.ts
+++ app/src/modules/users/users.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaModule } from 'src/config/prisma/prisma.module';
-import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, RolesGuard],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
